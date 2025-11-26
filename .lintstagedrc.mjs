e543ef36dc9c63bdab0ea6caf387f44b82fdcb2a
Oleksandr Zariechniev <158515671+ozariechniev@users.noreply.github.com>
2025-11-27 00:19:32 +0100

    Refactor lint-staged configuration

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,7 +1,5 @@
-import path from 'path';
-
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
+  `eslint --fix ${filenames.join(' ')}`;
 
 const config = {
   '*.{js,jsx,ts,tsx}': ['prettier --write', buildEslintCommand],
